23525549423da1f67f0d6baa022dc358c4052320
BorisDvn <dovonon@th-brandenburg.de>
2022-01-25 19:15:08 +0100

    NewsletterIT: DONE!

--- src/test/java/com/team/angular/interactiondesignapi/newsletter/NewsletterIT.java
+++ src/test/java/com/team/angular/interactiondesignapi/newsletter/NewsletterIT.java
@@ -12,11 +12,13 @@ import java.util.UUID;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.beans.SamePropertyValuesAs.samePropertyValuesAs;
 
 public class NewsletterIT extends ItBase {
 
-    Newsletter newsletter, newsletter1, newsletter2;
+    Newsletter newsletter;
 
     @BeforeEach
     public void setup() {
@@ -24,7 +26,6 @@ public class NewsletterIT extends ItBase {
 
         newsletter = buildNewsletter();
         newsletter = newsletterRepository.save(newsletter);
-        //, newsletter1, newsletter2;
     }
 
     @AfterEach
@@ -75,12 +76,88 @@ public class NewsletterIT extends ItBase {
         Assertions.assertEquals("Request processing failed; nested exception is java.lang.Exception: This email is already subscribed", ex.getLocalizedMessage());
     }
 
+    @Test
+    public void unsubscribeNewsletter() {
 
-    //test email not null
-    //test email correct
+        boolean statusBefore = newsletter.isStatus(); // true
 
-    //unsubcribe
+        given()
+                .contentType(ContentType.JSON)
+                .body(newsletter)
+                .log().body()
+                .put("/newsletters/unsubscribe/" + newsletter.getId())
+                .then()
+                .log().body()
+                .statusCode(200);
 
-    //update
-    //delete
+        assertThat(statusBefore, is(newsletter.isStatus()));//is(newsletter.isStatus())
+    }
+
+    //
+    @Test
+    public void updateNewsletter() {
+        Newsletter create = buildNewsletter();
+        UUID id_newsletter = UUID.fromString(
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(create)
+                        .log().body()
+                        .post("/newsletters/subscribe")
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().body().path("id"));
+
+        Newsletter newsletter = newsletterRepository.findById(id_newsletter).get();
+
+        Newsletter update = buildNewsletter();
+        update.setId(newsletter.getId());
+        update.setEmail(create.getEmail());
+        update.setStatus(create.isStatus());
+
+        UUID id = UUID.fromString(
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(newsletter)
+                        .log().body()
+                        .put("/newsletters")
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().body().path("id"));
+
+        Newsletter newsletter_ = newsletterRepository.findById(id).get();
+        assertThat(newsletter_, samePropertyValuesAs(update));
+    }
+
+    @Test
+    public void listNewsletter() {
+        Newsletter newsletter1 = buildNewsletter();
+        newsletter1 = newsletterRepository.save(newsletter1);
+
+        given()
+                .contentType(ContentType.JSON)
+                .log().body()
+                .get("/newsletters")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("id", containsInAnyOrder(newsletter.getId().toString(), newsletter1.getId().toString()))
+                .body("email", containsInAnyOrder(newsletter.getEmail(), newsletter1.getEmail()))
+                .body("status", containsInAnyOrder(newsletter.isStatus(), newsletter1.isStatus()));
+    }
+
+    @Test
+    public void deleteNewsletter() {
+
+        given()
+                .contentType(ContentType.JSON)
+                .log().body()
+                .delete("/newsletters/" + newsletter.getId())
+                .then()
+                .log().body()
+                .statusCode(200);
+
+        assertThat(newsletterRepository.findById(newsletter.getId()).isPresent(), is(false));
+    }
 }
