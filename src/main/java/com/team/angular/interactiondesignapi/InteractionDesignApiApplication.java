4a1c3236357cd56bf4b72a835847b27b155e8ad6
BorisDvn <dovonon@th-brandenburg.de>
2022-02-10 16:18:01 +0100

    add default admin

--- src/main/java/com/team/angular/interactiondesignapi/InteractionDesignApiApplication.java
+++ src/main/java/com/team/angular/interactiondesignapi/InteractionDesignApiApplication.java
@@ -1,16 +1,27 @@
 package com.team.angular.interactiondesignapi;
 
+import com.team.angular.interactiondesignapi.models.Admin;
+import com.team.angular.interactiondesignapi.services.AdminService;
+import net.sf.jasperreports.engine.JRException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import net.sf.jasperreports.engine.JRException;
-
 @SpringBootApplication
-public class InteractionDesignApiApplication {
+public class InteractionDesignApiApplication implements CommandLineRunner {
 
-	public static void main(String[] args) throws JRException {
-		SpringApplication.run(InteractionDesignApiApplication.class, args);	
-		
-	}
+    @Autowired
+    AdminService adminService;
+
+    public static void main(String[] args) throws JRException {
+        SpringApplication.run(InteractionDesignApiApplication.class, args);
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        adminService.addAdmin(new Admin(null, "superadmin", "superadmin", "superadminkennwort", "superadmin@universitytravelclub.de", null, null, null));
+    }
 
 }
