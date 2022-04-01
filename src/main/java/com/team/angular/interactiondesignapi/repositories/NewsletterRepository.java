3686d45027ea9258ac2fc6c4a5aa0c7531e12c91
BorisDvn <dovonon@th-brandenburg.de>
2022-04-01 20:45:18 +0200

    email to subscribers:ok

--- src/main/java/com/team/angular/interactiondesignapi/repositories/NewsletterRepository.java
+++ src/main/java/com/team/angular/interactiondesignapi/repositories/NewsletterRepository.java
@@ -2,8 +2,10 @@ package com.team.angular.interactiondesignapi.repositories;
 
 import com.team.angular.interactiondesignapi.models.Newsletter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -11,4 +13,7 @@ public interface NewsletterRepository extends JpaRepository<Newsletter, UUID> {
     boolean existsNewsletterByEmail(String email);
 
     boolean existsNewsletterByEmailAndIdIsNot(String email, UUID id);
+
+    @Query("select n.email from Newsletter n where n.status = true")
+    List<String> findAllByStatusIsTrue();
 }
